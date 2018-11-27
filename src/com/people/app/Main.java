4c437b913208b29397701f223ed38d827507cd89
jordi <jboixbaro@gmail.com>
2018-11-27 13:00:01 +0100

    isAdult with new exercice

--- src/com/people/app/Main.java
+++ src/com/people/app/Main.java
@@ -3,8 +3,37 @@ package com.people.app;
 public class Main {
 
 	public static void main(String[] args) {
-		People p1;
+		People p1 = new People("EDU", 16);
+		People p2 = new People("FELIZ", 19);
+		People p3 = new People("NAVIDAD", 29);
+
+		People p4 = p1;
+
+		p1.setName("YA NO SOY EDU");
+		p4.setAge(10);
+
+		p3 = p2;
+
+		p3.setName("O SI");
+		p1 = p2;
+		p2 = p1;
+		p4.setAge(29);
+		printPeople(p1);
+		p1.setName("SOY EDU");
+
+		printPeople(p2);
+		p3.setName("VERANO");
+		p1 = p3;
 		
+		printPeople(p3);
+		p1.setAge(34);
+		p2 = p4;
+
+		printPeople(p4);
+	}
+	
+	private static void printPeople(People p) {
+		System.out.println("" + p.getName() + " Y SOY ADULTO? " + p.isAdult());
 	}
 
 }
--- src/com/people/app/People.java
+++ src/com/people/app/People.java
@@ -4,8 +4,28 @@ public class People {
 
 	private String name;
 	
-	public People(String name) {
+	private int age;
+	
+	public People(String name, int age) {
 		this.name=name;
+		this.age=age;
 	}
+
+	public boolean isAdult() {
+		return age>18;
+	}
+	
+	public void setAge(int age) {
+		this.age=age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 }
